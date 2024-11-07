1dd2c5721f9bda64fb711983a81feeef61da80af
IduZaToboy <baut2002@mail.ru>
2024-11-08 00:12:03 +0700

    Аннотирование в классе

--- modules/common/my_class.py
+++ modules/common/my_class.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 class Users(object):
     def __init__(self, id: int, telegram_id: int, masterkey_lifetime: datetime):
-        self.id = id
-        self.telegram_id = telegram_id
-        self.masterkey_lifetime = masterkey_lifetime
+        self.id: int = id
+        self.telegram_id: int = telegram_id
+        self.masterkey_lifetime: datetime = masterkey_lifetime
